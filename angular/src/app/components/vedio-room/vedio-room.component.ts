bdc1e9d7670defcbf4740dd1adb31467c90db856
xiaxia112 <33377370+xiaxia112@users.noreply.github.com>
2020-06-03 01:33:12 +0800

    Add video-room component, however, the js files not work in compiled angular app. e.g. The function createRoom() is marked as undefined, which has been defined in components/video-room/client.js.

--- angular/src/app/components/vedio-room/vedio-room.component.ts
+++ angular/src/app/components/vedio-room/vedio-room.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vedio-room',
   templateUrl: './vedio-room.component.html',
+  // javaScriptUrls:['./client.js', './index.js', 'socket.io.js'],
   styleUrls: ['./vedio-room.component.css']
 })
 export class VedioRoomComponent implements OnInit {
